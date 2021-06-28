67ca8c4e025193a6d7784077b07b1eed21f936b1
wangkaman <wangkaman2016@163.com>
2021-06-28 20:53:28 +0800

    85%

---
+++ pages/article/components/article-comments.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <form class="card comment-form">
+      <div class="card-block">
+        <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
+      </div>
+      <div class="card-footer">
+        <img :src="user.image" class="comment-author-img" />
+        <button class="btn btn-sm btn-primary">
+          Post Comment
+        </button>
+      </div>
+    </form>
+    
+    <div class="card" v-for="comment in comments" :key="comment.id">
+      <div class="card-block">
+        <p class="card-text">{{ comment.body }}</p>
+      </div>
+      <div class="card-footer">
+        <nuxt-link class="comment-author" :to="{
+          name: 'profile',
+          params: { username: comment.author.username }
+        }">
+          <img :src="comment.author.image" class="comment-author-img" />
+        </nuxt-link>
+        &nbsp;
+        <nuxt-link class="comment-author" :to="{
+          name: 'profile',
+          params: { username: comment.author.username }
+        }">
+          {{ comment.author.username }}
+        </nuxt-link>
+        <span class="date-posted">{{ comment.createdAt | date('MMM DD, YYYY') }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// api
+import { getComments } from '@/api/article.js'
+// vuex
+import { mapState } from 'vuex'
+export default {
+  name: 'ArticleCommonts',
+  props: {
+    article: {
+      type: Object,
+      required: true,
+    }
+  },
+  data () {
+    return {
+      comments: [], // 评论列表
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  methods: {
+    async loadComments () {
+      const { data } = await getComments(this.article.slug)
+      this.comments = data.comments
+    },
+  },
+  mounted () {
+    this.loadComments()
+  },
+}
+</script>
+
+<style>
+
+</style>
--- pages/article/index.vue
+++ pages/article/index.vue
@@ -6,24 +6,7 @@
 
         <h1>{{ article.title }}</h1>
 
-        <div class="article-meta">
-          <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <ArticleMeta :article="article" />
 
       </div>
     </div>
@@ -31,85 +14,19 @@
     <div class="container page">
 
       <div class="row article-content">
-        <div class="col-md-12">
-          {{ article.body }}
-        </div>
+        <div class="col-md-12" v-html="article.body"></div>
       </div>
 
       <hr />
 
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <ArticleMeta :article="article" />
       </div>
 
       <div class="row">
-
         <div class="col-xs-12 col-md-8 offset-md-2">
-
-          <form class="card comment-form">
-            <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
-            </div>
-            <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              <button class="btn btn-sm btn-primary">
-              Post Comment
-              </button>
-            </div>
-          </form>
-          
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-              <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-              </span>
-            </div>
-          </div>
-          
+          <ArticleComments :article="article" />
         </div>
-
       </div>
 
     </div>
@@ -118,16 +35,36 @@
 </template>
 
 <script>
+// api
 import { getArticle } from '@/api/article.js'
+// markdown-render
+import MarkdownIt from 'markdown-it'
+// components
+import ArticleMeta from './components/article-meta.vue'
+import ArticleComments from './components/article-comments.vue'
 export default {
   middleware: 'authenticated',
   name: 'ArticleIndex',
+  components: { ArticleMeta, ArticleComments },
   async asyncData({ params }) {
     const { data } = await getArticle(params.slug)
+
+    const { article } = data
+    const md = new MarkdownIt()
+    article.body = md.render(article.body)
+
     return {
-      article: data.article
+      article,
     }
   },
+  head () {
+    return {
+      title: `${this.article.title} - realworld`,
+      meta: [
+        { hid: 'description', name: 'description', content: this.article.description }
+      ]
+    }
+  }
 }
 </script>
 
